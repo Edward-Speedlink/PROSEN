194ba2b76bfec098f8a19be92d2e345e649d0d7e
Edward Ndiyo <edward.ndiyo@speedlinkng.com>
2025-09-29 16:56:56 +0100

    Public IP Endpoint4

--- src/services/api.js
+++ src/services/api.js
@@ -4,7 +4,7 @@ import axios from "axios";
 const api = axios.create({
   // baseURL: "http://localhost:5000/api/", // http://192.168.10.28:5000/// change this to your backend base URL
   // baseURL: " http://192.168.10.27:5000/api/", // http://192.168.10.28:5000/// change this to your backend base URL
-  baseURL: "https://prosen1.speedlinkng.com/api", // http://192.168.10.28:5000/// change this to your backend base URL
+  baseURL: "https://prosen1.speedlinkng.com:443/api", // http://192.168.10.28:5000/// change this to your backend base URL
 
   headers: {
     "Content-Type": "application/json",
